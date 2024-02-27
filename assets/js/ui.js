4d3ccaa7b443d81d9a1ae769c6dd004c634f7ff6
neBill <63512444+neBill@users.noreply.github.com>
2024-02-27 16:25:46 +0700

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -575,15 +575,19 @@ function setTestHistory(testId) {
 
 function chooseTest(testId) { 
 
-  const testList = {
-    level_5b : test_5b,     
-    level_6b : test_6b,
-    level_6e : test_6e,
+  const testList = {  
+
+    b5 : test_5b,
+    s5 : test_5s,
+    e5 : test_5e,
+    b6 : test_6b,
+    s6 : test_6s,
+    e6 : test_6e,     
     ot : test_ot,
     ot_maxim : test_maxim,
     micro_5 : micro_5,
     micro_6 : micro_6,
-    temp_test : test, 
+     
   }
   return testList[testId];
 }
