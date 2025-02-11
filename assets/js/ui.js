359072c3745d9463c45f62cc4f0e434c96795e8e
neBill <63512444+neBill@users.noreply.github.com>
2025-02-11 08:34:16 +0000

    add modal window

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -729,7 +729,12 @@ document.addEventListener("click", function (event) {
 
     let elemId = event.target.id
 
-    alert(currentTest.test[elemId][1]);
+    //alert(currentTest.test[elemId][1]);
+    // Get the modal
+    //var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    span.textContent = currentTest.test[elemId][1];
 
   }
 
@@ -737,4 +742,30 @@ document.addEventListener("click", function (event) {
 
 
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   
