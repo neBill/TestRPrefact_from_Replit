9de9b980113d6fb6fb3f99da7d08307dd827fd12
neBill <63512444+neBill@users.noreply.github.com>
2025-02-20 23:35:13 +0700

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -584,6 +584,7 @@ function chooseTest(testId) {
     e5 : test_5e,
     e6 : test_6e,
     ot : test_ot,
+    ot_2025 : test_otper,
     ot_maxim : test_maxim,
     micro_5 : micro_5,
     micro_6 : micro_6,
