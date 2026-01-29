b3952b6524657d71a2dce428100413ba7b693449
neBill <63512444+neBill@users.noreply.github.com>
2026-01-29 10:11:19 +0000

    add new questions

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -209,6 +209,8 @@ function getTestList() {
     tg : [test_tg, "Конд. и ТГ"],    
     fakel : [test_fakel, "Факелы"],  
     gen : [test_ot26, "ОТ общие"],
+    mob : [test_mob, "Мобильные обходы"],
+    
    
   }
 
@@ -226,6 +228,7 @@ function getTestList() {
     ph : [test_ph, "ПХЦ"],
     eh : [test_eh, "ЭХЦ"], 
     gen : [test_ot26, "ОТ общие"],
+    mob : [test_mob, "Мобильные обходы"],
    
   }
  
